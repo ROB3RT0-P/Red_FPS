e8c853dcdee88873cf2c23c6f02bcf051c672166
Rob Parry <rob_parry@outlook.com>
2023-10-23 12:51:30 +0100

    Initial commit
    files and entry point

---
+++ Red_OpenGL/Red_OpenGL/red_main.cpp
@@ -0,0 +1,64 @@
+//----------------------------
+// red_main - RJP - 22/10/2023
+//----------------------------
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+// GLFW window and input callbacks
+GLFWwindow* window;
+int screenWidth = 800;
+int screenHeight = 600;
+
+void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
+
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    // Set GLFW options and create a window
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(screenWidth, screenHeight, "OpenGL Game", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, KeyCallback);
+
+    // Initialize GLEW
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+
+    // OpenGL setup
+    glViewport(0, 0, screenWidth, screenHeight);
+    glEnable(GL_DEPTH_TEST);
+
+    // Game loop
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        // Render here
+
+        glfwSwapBuffers(window);
+    }
+
+    // Cleanup and exit
+    glfwTerminate();
+    return 0;
+}
